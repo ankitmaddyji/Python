e5f909ae24af0829b6492457131e3f4e68029f6f
Mokshablr <arjunrmaniyani@gmail.com>
2022-10-13 17:29:56 +0530

    My PR

---
+++ mine.py
@@ -0,0 +1,16 @@
+''''
+Author: Mokshablr
+Title: Character counter in string
+
+'''
+st=input("Enter a string: ")
+ch=input("Enter a character: ")
+leng=len(st)
+ctr=0
+
+for i in range(0,leng):
+    current=st[i]
+    if current==ch:
+        ctr+=1
+
+print(ch,"is in the string",ctr,"times")
